11fc3a71d92da84006f4b7ae15d89444bd617878
kazi.hassan <kazi.hassan@CRT-13PY903.alexgov.net>
2022-04-11 12:40:28 -0400

    updated

---
+++ src/application/App.java
@@ -0,0 +1,17 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		
+		while(true) {
+		System.out.println("I am stupid");
+		System.out.println("now some linux not clear on lot of things");
+		
+		
+		
+		}
+	}
+
+}
